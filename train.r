8470d2f7a9fa8b8baef3285fd298d0096d68914c
Your Name <you@example.com>
2026-01-14 07:54:21 +0100

    feat: removed temp predictor

--- train.r
+++ train.r
@@ -3,7 +3,7 @@ options(warn=1)
 train_chap <- function(csv_fn, model_fn) {
   df <- read.csv(csv_fn)
   df$disease_cases[is.na(df$disease_cases)] <- 0
-  model <- lm(disease_cases ~ rainfall + mean_temperature, data = df)
+  model <- lm(disease_cases ~ rainfall, data = df)
   saveRDS(model, file=model_fn)
 }
 
